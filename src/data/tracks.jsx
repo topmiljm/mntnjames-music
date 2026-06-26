d5d2467292e13f02724b018e214a1aa19894801e
James <james.m.topmiller@gmail.com>
2026-06-26 14:18:37 -0700

    wolf demo artwork-1

--- src/data/tracks.jsx
+++ src/data/tracks.jsx
@@ -15,7 +15,7 @@ export const albums = [
     title: 'Wolf Demos',
     year: 2025,
     color: '#3b5e3a',
-    coverArt: '/images/wolf demos artwork-2.png'
+    coverArt: '/images/wolf demos artwork-1.png'
   },
   {
     id: 3,
